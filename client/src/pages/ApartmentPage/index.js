8577586a9e7b1c76f64babb6d6868df8a0aac665
yura <uramen12@gmail.com>
2017-06-22 17:03:16 +0000

    temp

--- client/src/pages/ApartmentPage/index.js
+++ client/src/pages/ApartmentPage/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import FontAwesome from 'react-fontawesome';
-import { Link } from 'react-router';
 import './assets/component.css';
+import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome'
+import Tooltip from 'rc-tooltip';
+import { Link } from 'react-router';
+import 'rc-tooltip/assets/bootstrap.css';
 
+import _ from 'lodash';
 
-export default ({data, loading}) => {
-  return (
-    <div className="apartment-details">
+export default class Apartment extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+componentWillReceiveProps(nextProps) {
+  this.apartment = _.filter(nextProps.data.apartments, I => I.id === this.props.params.id);
+}
+
+  render() {
+    return (
+       <div className="apartment-details">
+       {this.apartment &&
       <div className="info">
         <div>
           <h2>Детальна сторінка</h2>
@@ -14,8 +28,12 @@ export default ({data, loading}) => {
         </div>
       </div>
       <div className="detail-white">
-        
+        <div className="images">
+          {this.apartment.images.map(src => (<img src={src}/>))}
+        </div>
       </div>
+       }
     </div>
-  )
-};
+    );
+  }
+}
